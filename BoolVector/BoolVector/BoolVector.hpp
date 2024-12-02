5bbd3592f271104861526af08577bcbed9432d8b
MaximSakharov <145155919+MaximSakharov@users.noreply.github.com>
2024-12-02 23:04:42 +0300

    Add files via upload

--- BoolVector/BoolVector/BoolVector.hpp
+++ BoolVector/BoolVector/BoolVector.hpp
@@ -1,381 +1,80 @@
+#ifndef BOOL_VECTOR_HPP
+#define BOOL_VECTOR_HPP
+
 #include <iostream>
+#include "Exception.hpp"
 
 using namespace std;
 
-class BoolVector
-{
+class BoolVector {
 private:
    bool* elements;
    size_t size;
    size_t capacity;
+
 public:
-   class Iterator 
-   {
+   class Iterator {
    private:
-      bool* current; // Указатель на текущий элемент
+      bool* current;
+
    public:
-      Iterator(bool* ptr) : current(ptr) {}
-
-      // Операторы разыменования
-      bool& operator*() 
-      {
-         return *current;
-      }
-
-      const bool& operator*() const 
-      {
-         return *current;
-      }
-
-      // Оператор инкремента
-      Iterator& operator++() 
-      {
-         ++current;
-         return *this;
-      }
-
-      // Оператор сравнения
-      bool operator!=(const Iterator& other) const 
-      {
-         return current != other.current;
-      }
+      Iterator(bool* ptr);
+      bool& operator*();
+      const bool& operator*() const;
+      Iterator& operator++();
+      Iterator operator++(int);
+      bool operator==(const Iterator& other) const;
+      bool operator!=(const Iterator& other) const;
    };
 
-   BoolVector(size_t capacity_= 0)
-      :capacity(capacity), size(0), elements(capacity ? new bool[capacity] : nullptr) {}
+   BoolVector(size_t capacity_ = 0);
+   BoolVector(bool* vector_, size_t capacity_);
+   ~BoolVector();
 
-   ~BoolVector()
-   {
-      if (elements)
-         delete[] elements;
-   }
+   BoolVector& operator=(const BoolVector& other_);
+   BoolVector(const BoolVector& other_);
 
+   bool& operator[](size_t i) const;
+   bool& operator[](size_t i);
+   bool At(size_t pos_);
+   bool Front();
+   bool Back();
 
-   BoolVector& operator=(const BoolVector& other_) 
-   {
-      if (this != &other_) 
-      {
-         delete[] elements;
-         size = other_.size;
-         capacity = other_.capacity;
-         elements = new bool[capacity];
+   void SetElement(size_t pos_, bool new_element_);
+   void PushBack(bool element_);
+   void PopBack();
 
-         for (size_t i = 0; i < size; ++i)
-            elements[i] = other_[i];
-      }
-      return *this;
-   }
-   BoolVector(const BoolVector& other_)
-      : size(other_.size), capacity(other_.capacity), elements(nullptr) 
-   {
-      if (capacity > 0) 
-      {
-         elements = new bool[capacity];
-         for (size_t i = 0; i < size; ++i)
-            elements[i] = other_[i];
-      }
-   }
+   BoolVector OrOperation(const BoolVector& other_) const;
+   BoolVector AndOperation(const BoolVector& other_) const;
+   bool operator==(const BoolVector& other_) const;
+   bool operator!=(const BoolVector& other_) const;
+   BoolVector operator!() const;
 
-   bool& operator[](size_t i) const
-   {
-      if (i >= size)
-         throw 1;
-
-      return elements[i];
-   }
-   bool& operator[](size_t i)
-   {
-      if (i >= size)
-         throw 1;
-
-      return elements[i];
-   }
-   bool At(size_t pos_)
-   {
-      if (pos_ >= size)
-         throw 1;
-
-      return elements[pos_];
-   }
-   bool Front()
-   {
-      if (size == 0)
-         throw 1;
-
-      return elements[0];
-   }
-   bool Back()
-   {
-      if (size == 0)
-         throw 1;
-
-      return elements[size - 1];
-   }
-   void SetElement(size_t pos_, bool new_element_)
-   {
-      if (pos_ >= size)
-         throw 1;
-
-      elements[pos_] = new_element_;
-   }
-
-   void PushBack(bool element_)
-   {
-      if (size < capacity)
-      {
-         elements[size++] = element_;
-         return;
-      }
-      if (capacity == 0)
-      {
-         elements = new bool;
-         capacity = size = 1;
-         elements[0] = element_;
-      }
-      else
-      {
-         bool* new_elements = new bool[capacity *= 2];
-         if (!new_elements)
-            return;
-
-         for (size_t i = 0; i < size; ++i)
-            new_elements[i] = elements[i];
-
-         delete[] elements;
-         elements = new_elements;
-
-         elements[size++] = element_;
-      }
-   }
-   void PopBack()
-   {
-      if (size == 0)
-         return;
-
-      --size;
-   }
-   void Insert(size_t pos_, bool element_)
-   {
-      if (pos_ > size - 1)
-         throw 1;
-
-      elements[pos_] = element_;
-   }
-
-   BoolVector OrOperation(const BoolVector& other_) const
+   BoolVector XorOperation(const BoolVector& other_) const 
    {
       if (size != other_.size)
-         throw 2;
+         throw SizeMismatchException();  // Исключение при несовпадении размеров
 
-      BoolVector result(size);
+      BoolVector result(size);  // Новый вектор для хранения результата
       result.size = size;
-
       for (size_t i = 0; i < size; ++i)
-      {
-         result[i] = (elements[i] || other_.elements[i]);
-      }
-
-      return result;
-   }
-   BoolVector AndOperation(const BoolVector& other_) const
-   {
-      if (size != other_.size)
-         throw 2;
-
-      BoolVector result(size);
-      result.size = size;
-
-      for (size_t i = 0; i < size; ++i)
-      {
-         result[i] = (elements[i] && other_.elements[i]);
-      }
+         result[i] = (elements[i] != other_.elements[i]);  // XOR операция
 
       return result;
    }
 
-   /*
-      BoolVector operator^(const BoolVector& other) const {
-        if (size != other.size) {
-            throw std::invalid_argument("Vectors must be of the same size");
-        }
-        BoolVector result(size);
-        for (size_t i = 0; i < size; ++i) {
-            result.set(i, get(i) ^ other.get(i));
-        }
-        return result;
-    }
-   */
+   void Print();
+   size_t ScalarProduct(const BoolVector& other_) const;
+   BoolVector Concatenate(const BoolVector& other_) const;
+   BoolVector ShiftLeftOperation(int n) const;
+   BoolVector ShiftRightOperation(int n) const;
+   BoolVector SubvectorExtraction(size_t left_, size_t right_);
 
-   bool operator==(const BoolVector& other_) const
-   {
-      if (size != other_.size)
-         return false;
-
-      for (size_t i = 0; i < size; ++i)
-         if (elements[i] != other_[i])
-            return false;
-
-      return true;
-   }
-   bool operator!=(const BoolVector& other_) const
-   {
-      return !(*this == other_);
-   }
-
-   BoolVector operator!() const
-   {
-      BoolVector result(size);
-
-      for (size_t i = 0; i < size; ++i)
-         result[i] = !elements[i];
-
-      return result;
-   }
-   void Print()
-   {
-      cout << "< ";
-      for (size_t i = 0; i < size - 1; ++i)
-         cout << elements[i] << ", ";
-
-      cout << elements[size - 1];
-
-      cout << " >";
-   }
-   /*
-   int operator*(const BoolVector& other_) const
-   {
-      if (size != other_.size)
-         throw 2;
-
-      double result = 0;
-      for (size_t i = 0; i < size; ++i) {
-         result += elements[i] * other_.elements[i];
-      }
-      return result;
-   }
-   */
-   size_t ScalarProduct(const BoolVector& other_) const 
-   {
-      if (size != other_.size)
-         throw 2;
-
-      size_t result = 0;
-      for (size_t i = 0; i < size; ++i)
-         result += (elements[i] && other_.elements[i]);
-      
-      return result;
-   }
-   BoolVector Concatenate(const BoolVector& other_) const
-   {
-      //bool *arr = new bool[size + other_.size];
-
-      BoolVector result(size + other_.size);
-
-      for (size_t i = 0; i < size; ++i)
-         result.elements[i] = elements[i];
-      for (size_t i = 0; i < other_.size; ++i)
-         result.elements[i + size] = other_.elements[i];
-
-      return result;
-   }
-   BoolVector ShiftLeftOperation(size_t n) const
-   {
-      if (n < 0)
-         throw 3;
-
-      BoolVector result(size + n);
-      for (size_t i = 0; i < size; ++i) {
-         result[i] = elements[i];
-      }
-
-      return result;
-   }
-
-   BoolVector ShiftRightOperation(size_t n) const
-   {
-      if (n < 0) 
-         throw 3;
-      
-      if (n >= size)
-         return BoolVector(0);
-
-      bool* arr = new bool[size + n];
-      BoolVector result(size - n);
-
-      for (size_t i = 0; i < size - n; ++i)
-         result[i] = elements[n + i];
-
-      return result;
-   }
-
-   // Метод для получения итератора на первый элемент
-   Iterator Begin() 
-   {
-      return Iterator(elements);
-   }
-
-   // Метод для получения итератора на элемент после последнего
-   Iterator End() 
-   {
-      return Iterator(elements + size);
-   }
+   Iterator Begin();
+   Iterator End();
 
    friend ostream& operator<<(ostream& stream, const BoolVector& vector_);
    friend istream& operator>>(istream& stream, BoolVector& vector_);
-
 };
 
-ostream& operator<<(ostream& stream, const BoolVector& vector_)
-{
-   stream << "< ";
-
-   size_t vector_size = vector_.size - 1;
-
-   for (size_t i = 0; i < vector_size; ++i)
-      stream << vector_[i] << ", ";
-   
-   stream << vector_[vector_size];
-
-   stream << " >";
-
-   return stream;
-}
-
-istream& operator>>(std::istream& stream, BoolVector& vector_) 
-{
-   if (vector_.elements)
-   {
-      delete[] vector_.elements;
-      vector_.size = 0;
-      vector_.elements = new bool[vector_.capacity];
-   }
-
-   char ch;
-   bool a;
-
-   // Ожидаем символ '<'
-   stream >> ch;
-   if (ch != '<') 
-   {
-      throw 4;
-   }
-
-   // Читаем элементы до символа '>'
-   while (stream >> a) 
-   {
-      vector_.PushBack(a);
-
-      // Ожидаем либо '>' либо ',' чтобы продолжить
-      stream >> ch;
-      if (ch == '>') 
-      {
-         break; // Выходим из цикла, если встретили '>'
-      }
-      else if (ch != ',') 
-      {
-         throw 4;
-      }
-   }
-
-   return stream;
-}
+#endif // BOOL_VECTOR_HPP
--- BoolVector/BoolVector/main.cpp
+++ BoolVector/BoolVector/main.cpp
@@ -1,28 +1,153 @@
 #include <iostream>
 
 #include "BoolVector.hpp"
+#include "Exception.hpp"
+#include <locale.h>
 
 using namespace std;
 
 
+int main() 
+{
+   setlocale(LC_ALL, "RUS");
+   // 1. Создание BoolVector с заданной ёмкостью
+   BoolVector bv1(5);  // Создаем вектор с ёмкостью 5
+
+   // 2. Использование оператора PushBack для добавления элементов
+   bv1.PushBack(true);
+   bv1.PushBack(false);
+   bv1.PushBack(true);
+   bv1.PushBack(false);
+   bv1.PushBack(true);
+
+   // 3. Вывод вектора
+   cout << "Vector bv1: " << bv1 << endl;
+
+   // 4. Использование конструктора копирования
+   BoolVector bv2 = bv1;
+   cout << "Vector bv2 (копия bv1): " << bv2 << endl;
+
+   // 5. Использование оператора присваивания
+   BoolVector bv3(3);  // Создаём новый вектор
+   bv3 = bv1;  // Присваиваем bv1 в bv3
+   cout << "Vector bv3 (присваивание bv1): " << bv3 << endl;
+
+   // 6. Использование оператора индексирования
+   try {
+      cout << "Element at index 2 in bv1: " << bv1[-1] << endl;
+      cout << "Element at index 5 in bv1 (ошибка): " << bv1[5] << endl;  // Это вызовет исключение
+   }
+   catch (const IndexOutOfRangeException& e) {
+      e.Print();
+   }
+
+   // 7. Применение операций логического И и ИЛИ
+   BoolVector bv4(5);  // Создадим другой вектор для операции
+   bv4.PushBack(true);
+   bv4.PushBack(true);
+   bv4.PushBack(false);
+   bv4.PushBack(true);
+   bv4.PushBack(false);
+
+   cout << "Vector bv4: " << bv4 << endl;
+
+   BoolVector bv5 = bv1.OrOperation(bv4);
+   cout << "Result of OR operation between bv1 and bv4: " << bv5 << endl;
+
+   BoolVector bv6 = bv1.AndOperation(bv4);
+   cout << "Result of AND operation between bv1 and bv4: " << bv6 << endl;
+
+   // 8. Использование оператора побитового отрицания (!)
+   BoolVector bv7 = !bv1;
+   cout << "Result of NOT operation on bv1: " << bv7 << endl;
+
+   // 9. Использование метода SubvectorExtraction
+   try {
+      BoolVector sub = bv1.SubvectorExtraction(1, 3);  // Извлекаем элементы с индекса 1 по 3
+      cout << "Subvector from bv1 (1 to 3): " << sub << endl;
+   }
+   catch (const IndexOutOfRangeException& e) {
+      e.Print();
+   }
+
+   // 10. Использование оператора сдвига влево (ShiftLeftOperation)
+   BoolVector bv8 = bv1.ShiftLeftOperation(4);
+   cout << "Result of left shift by 2: " << bv8 << endl;
+
+   // 11. Использование оператора сдвига вправо (ShiftRightOperation)
+   BoolVector bv9 = bv1.ShiftRightOperation(4);
+   cout << "Result of right shift by 2: " << bv9 << endl;
+
+   // 12. Проверка на пустоту вектора (метод Front и Back)
+   try {
+      cout << "First element in bv1 (Front): " << bv1.Front() << endl;
+      cout << "Last element in bv1 (Back): " << bv1.Back() << endl;
+   }
+   catch (const EmptyVectorException& e) {
+      e.Print();
+   }
+
+   // 13. Перебор с использованием итератора
+   cout << "Iterating over bv1 using iterator: ";
+   for (BoolVector::Iterator it = bv1.Begin(); it != bv1.End(); ++it) {
+      cout << *it << " ";  // Выводим каждый элемент
+   }
+   cout << endl;
+
+   // 14. Тестирование операции сравнения
+   if (bv1 == bv2) {
+      cout << "bv1 и bv2 одинаковы" << endl;
+   }
+   else {
+      cout << "bv1 и bv2 разные" << endl;
+   }
+
+   // 15. Тестирование конкатенации двух векторов
+   BoolVector bv10 = bv1.Concatenate(bv4);
+   cout << "Concatenated bv1 and bv4: " << bv10 << endl;
+
+   // 16. Тестирование скалярного произведения
+   size_t scalarProduct = bv1.ScalarProduct(bv4);
+   cout << "Scalar product of bv1 and bv4: " << scalarProduct << endl;
+
+   return 0;
+}
+
+/*
 int main()
 {
+   setlocale(LC_ALL, "RUS");
+
+   bool arr[5] = { 1, 1, 1,0,1 };
+
    try
    {
       BoolVector a;
+      BoolVector b;
 
       cin >> a;
-
+      cin >> b;
       //a.Print();
       //cout << endl;
-      cout << a.Front();
+      //cout << a.Front();
 
       cout << a << endl;
+      cout << b << endl;
 
-      a.PopBack();
-      cout << a << endl;
+      BoolVector res = a.Concatenate(b);
 
-      /*
+      cout << res << endl;
+
+      //BoolVector res2;
+
+      //cin >> res2;
+
+      //cout << res2 << endl;
+
+      //a.PopBack();
+     //cout << a << endl;
+
+      
       BoolVector b(3);
       b.PushBack(0);
       b.PushBack(1);
@@ -39,14 +164,14 @@ int main()
       c.Print();
       cout << endl;
       d.Print();
-      */
+      
 
    }
-   catch (const int)
+   catch (const Exception& e)
    {
-      printf("Error!");
+      e.Print();
    }
-   /*
+   
    bool* arr1 = new bool[3];
    arr1[0] = true;
    arr1[1] = false;
@@ -63,12 +188,12 @@ int main()
    a.Print();
    b.Print();
 
-   BoolVector c = a >> 1;
 
    c.Print();
-   */
+   
 
 
       return 0;
    
 }
+*/
